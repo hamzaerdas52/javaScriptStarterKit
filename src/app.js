509963611eec3002f15ef3828b7d73c0caa859bb
Hamza <hamza05259@gmail.com>
2023-06-09 22:13:01 +0300

    1.Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,20 @@
 console.log("Merhaba Kodlama.io")
+console.log("Hamza")
 
-//JS type safe değildir
-let dolarBugun = 9.30
+// JS type safe değildir
 
-let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
-
+let dolarDun = 23.10
+let dolarBugun = 23.20
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
-
+const euroDun = 24.50
+//euroDun = 24
 console.log(euroDun)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
-}
-console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi"]
 console.log(konutKredileri)
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    const element = konutKredileri[i]
+    console.log(element)
+}
